cbc6e30923338bd54a22f29926cd0d6b2096310a
Alexandr-AD <a.alexandr.13@yandex.ru>
2022-08-14 18:48:19 +0300

    solve

--- tasks/text.py
+++ tasks/text.py
@@ -10,13 +10,15 @@ def find_shortest_longest_word(text: str) -> tuple[Optional[str], Optional[str]]
     В переданном тексте вернуть слово имеющее наименьшую и наибольшую длину.
     Если такого слова нет - вернуть None
     """
-    if len(text) == 0:
-        return (None, None)
-    else:
-        if re.fullmatch("\s{1,}", text):
-            return (None, None)
-        else:
-            testtxt = re.split("\s{1,}", text)
-            ans = (min(testtxt, key=len), max(testtxt, key=len))
-        return ans
+    words = text.split()
+    return (min(words, key=len), max(words, key=len)) if words else (None, None)
+    # if len(text) == 0:
+    #     return (None, None)
+    # else:
+    #     if re.fullmatch("\s{1,}", text):
+    #         return (None, None)
+    #     else:
+    #         testtxt = re.split("\s{1,}", text)
+    #         ans = (min(testtxt, key=len), max(testtxt, key=len))
+    #     return ans
 
